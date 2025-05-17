34c0357bcbc18cbd01d5abc3a868a8f4062b5645
Jrom2003 <jgromanow@gmail.com>
2025-05-17 10:56:35 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io');
 const path = require('path');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
@@ -12,18 +12,10 @@ app.use(express.static(path.join(__dirname, '../client')));
 
 io.on('connection', (socket) => {
   console.log('user connected:', socket.id);
-
-  socket.on('join-room', (roomCode, playerName) => {
-    socket.join(roomCode);
-    console.log(`${playerName} joined room ${roomCode}`);
-    io.to(roomCode).emit('player-joined', { id: socket.id, name: playerName });
-  });
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected:', socket.id);
+  socket.on('join-room', (room, name) => {
+    socket.join(room);
+    io.to(room).emit('player-joined', { name });
   });
 });
 
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
